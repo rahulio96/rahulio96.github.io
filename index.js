6c68054ae07a56ffd8e1156f61adcc2c638b73e6
rahulio96 <rahulgithub96@gmail.com>
2024-01-30 14:24:04 -0800

    Added a footer with links to GitHub and LinkedIn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 const burger = document.querySelector(".burger");
-const nav = document.querySelector("nav ul");
+const nav = document.querySelector("header nav ul");
 
 burger.addEventListener("click", () => {
     burger.classList.toggle("change");
     nav.classList.toggle("show");
 });
 
-document.querySelectorAll("nav ul li a").forEach(e => 
+document.querySelectorAll("header nav ul li a").forEach(e => 
     e.addEventListener("click", () => {
         burger.classList.toggle("change");
         nav.classList.toggle("show");
